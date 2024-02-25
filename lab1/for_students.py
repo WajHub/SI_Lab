8de980d9829a30c1049a75fa91de2ea21a6f3a39
Hubert Wajda <hubert.wajda03@gmail.com>
2024-02-25 22:43:11 +0100

    macierz obserwacji

---
+++ lab1/for_students.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from data import get_data, inspect_data, split_data
+
+data = get_data()
+inspect_data(data)
+
+train_data, test_data = split_data(data)
+
+# Simple Linear Regression
+# predict MPG (y, dependent variable) using Weight (x, independent variable) using closed-form solution
+# y = theta_0 + theta_1 * x - we want to find theta_0 and theta_1 parameters that minimize the prediction error
+
+# We can calculate the error using MSE metric:
+# MSE = SUM (from i=1 to n) (actual_output - predicted_output) ** 2
+
+# get the columns
+y_train = train_data['MPG'].to_numpy().reshape(-1,1) # Zamienamy na macierz o jednej kolumnie
+x_train = train_data['Weight'].to_numpy().reshape(-1,1)
+
+ones = np.ones((x_train.shape[0],1)) # Tworzymy macierz jedynek (jedna kolumna) - wyrazy wolne
+x_train_ = np.hstack((ones,x_train)) # Laczymy
+
+# print(x_train_.shape)
+# print(x_train_[:,:])
+
+
+y_test = test_data['MPG'].to_numpy()
+x_test = test_data['Weight'].to_numpy()
+
+# TODO: calculate closed-form solution
+theta_0 = 0
+theta_1 = 0
+
+theta_best = [theta_0, theta_1]
+
+# TODO: calculate error
+
+# plot the regression line
+x = np.linspace(min(x_test), max(x_test), 100)
+y = float(theta_best[0]) + float(theta_best[1]) * x
+plt.plot(x, y)
+plt.scatter(x_test, y_test)
+plt.xlabel('Weight')
+plt.ylabel('MPG')
+plt.show()
+
+# TODO: standardization
+
+# TODO: calculate theta using Batch Gradient Descent
+
+# TODO: calculate error
+
+# plot the regression line
+x = np.linspace(min(x_test), max(x_test), 100)
+y = float(theta_best[0]) + float(theta_best[1]) * x
+plt.plot(x, y)
+plt.scatter(x_test, y_test)
+plt.xlabel('Weight')
+plt.ylabel('MPG')
+plt.show()
