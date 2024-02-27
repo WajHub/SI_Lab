8030945023d5e8ec04409b6f0a9a09358b9e204f
Hubert Wajda <hubert.wajda03@gmail.com>
2024-02-27 14:05:20 +0100

    calculate form solution and erro

--- lab1/for_students.py
+++ lab1/for_students.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 
 from data import get_data, inspect_data, split_data
 
+
+def predict(X, theta):
+    return np.dot(X, theta)  # Iloczy wektorowy
+
+def getThetaClosedSolution(X, Y):
+    Y = np.mat(Y)
+    return np.linalg.pinv(np.dot(X.T, X)) * np.dot(X.T, Y) # Wynik wzoru 1.13
+
+
 data = get_data()
 inspect_data(data)
 
@@ -16,26 +25,36 @@ train_data, test_data = split_data(data)
 # MSE = SUM (from i=1 to n) (actual_output - predicted_output) ** 2
 
 # get the columns
-y_train = train_data['MPG'].to_numpy().reshape(-1,1) # Zamienamy na macierz o jednej kolumnie
-x_train = train_data['Weight'].to_numpy().reshape(-1,1)
+y_train = train_data['MPG'].to_numpy().reshape(-1, 1)  # Zamienamy na macierz o jednej kolumnie
+x_train = train_data['Weight'].to_numpy().reshape(-1, 1)
 
-ones = np.ones((x_train.shape[0],1)) # Tworzymy macierz jedynek (jedna kolumna) - wyrazy wolne
-x_train_ = np.hstack((ones,x_train)) # Laczymy
-
-# print(x_train_.shape)
-# print(x_train_[:,:])
-
-
-y_test = test_data['MPG'].to_numpy()
-x_test = test_data['Weight'].to_numpy()
 
 # TODO: calculate closed-form solution
 theta_0 = 0
 theta_1 = 0
 
+ones = np.ones((x_train.shape[0], 1))  # Tworzymy macierz jedynek (jedna kolumna) - wyrazy wolne
+x_train_ = np.hstack((ones, x_train))  # Laczymy - otrzymujemy macierz obserwacji
+
+theta = getThetaClosedSolution(x_train_,y_train)
+theta_0 = theta [0,0] #b
+theta_1 = theta[1,0] #a
+
+y_test = test_data['MPG'].to_numpy()
+x_test = test_data['Weight'].to_numpy()
+
 theta_best = [theta_0, theta_1]
 
 # TODO: calculate error
+m = sum(len(wiersz) for wiersz in x_train)
+mse = 0
+for i in range(len(x_train)):
+    for j in range(len(x_train[i])):
+        mse=mse + ((theta_1*x_train[i,j]+theta_0)-y_train[i,j])**2
+mse = mse/m
+print("Calculate error MSE:")
+print(mse)
+
 
 # plot the regression line
 x = np.linspace(min(x_test), max(x_test), 100)
